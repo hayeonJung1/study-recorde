196972e19144ff7e33dc7a8ff1866bc139f8fab3
hayeonJung1 <chung210109@gmail.com>
2025-08-29 13:31:55 +0900

    upload study log

---
+++ JAVA/code/Test/src/mypack/Test01.java
@@ -0,0 +1,10 @@
+package mypack;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		//콘솔창에 “Hello world!” 문자열을 출력해주세요.
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ JAVA/code/Test/src/mypack/Test03.java
@@ -0,0 +1,13 @@
+package mypack;
+
+public class Test03 {
+	public static void main(String[] args) {
+		//프로필 만들기
+		String name="홍길동";
+		String studentId="20250000";
+		double tall=169.9;
+		System.out.println("이름 : "+name);
+		System.out.println("학번 : "+studentId);
+		System.out.println("신장 : "+tall);
+	}
+}
